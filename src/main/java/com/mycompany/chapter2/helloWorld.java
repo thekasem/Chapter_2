f2ca609c3ef5fd099623e8f695ab566f36d78a4f
kasem <kasem@10.2.80.86>
2014-12-21 11:06:43 +0700

    commit1

---
+++ src/main/java/com/mycompany/chapter2/helloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author kasem
+ */
+public class helloWorld {
+    public static void main(String[] args){
+     System.out.println("hello world");
+    }    
+}
